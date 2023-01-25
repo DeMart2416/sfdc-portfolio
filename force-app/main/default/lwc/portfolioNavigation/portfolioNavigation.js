25b96788096fb1696530e91b87d386eb95040c2a
Sean DesMarteau <sean.desmarteau@gusto.com>
2023-01-25 15:08:50 -0700

    Update Navigation

--- force-app/main/default/lwc/portfolioNavigation/portfolioNavigation.js
+++ force-app/main/default/lwc/portfolioNavigation/portfolioNavigation.js
@@ -2,15 +2,23 @@ import { LightningElement } from 'lwc';
 import IMAGES from '@salesforce/resourceUrl/images';
 
 export default class PortfolioNavigation extends LightningElement {
+	sections = [
+		{ Id: 1, Name: 'home' },
+		{ Id: 2, Name: 'about' },
+		{ Id: 3, Name: 'experience' },
+		{ Id: 4, Name: 'education' },
+		{ Id: 5, Name: 'projects' }
+	];
+
 	seanProfile = IMAGES + '/images/profile-pictures/sean.jpeg';
 
 	handleLinkClick(event) {
-        event.preventDefault();
+		event.preventDefault();
 
-        const navigateEvent = new CustomEvent('navigate', {
+		const navigateEvent = new CustomEvent('navigate', {
 			detail: event.target.dataset.targetSection
 		});
-        
+
 		this.dispatchEvent(navigateEvent);
 	}
 }
