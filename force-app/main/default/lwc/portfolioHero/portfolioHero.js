b7ee1d2490dd75c3de4ff3352eca2b03f1f290f5
Sean DesMarteau <sean.desmarteau@gusto.com>
2023-01-25 23:19:11 -0700

    Finalizing Navigation

--- force-app/main/default/lwc/portfolioHero/portfolioHero.js
+++ force-app/main/default/lwc/portfolioHero/portfolioHero.js
@@ -1,6 +1,3 @@
 import { LightningElement } from 'lwc';
-import IMAGES from "@salesforce/resourceUrl/images";
-
 export default class PortfolioLead extends LightningElement {
-    seanProfile = IMAGES + '/images/profile-pictures/sean.jpeg';
 }
--- force-app/main/default/lwc/portfolioNavigation/portfolioNavigation.js
+++ force-app/main/default/lwc/portfolioNavigation/portfolioNavigation.js
@@ -1,16 +1,41 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import IMAGES from '@salesforce/resourceUrl/images';
-
+import HAM_MENU from '@salesforce/resourceUrl/hamMenu';
+import HAM_MENU_CLOSE from '@salesforce/resourceUrl/hamMenuClose';
 export default class PortfolioNavigation extends LightningElement {
+	// Declare Static Resources
+	seanProfile = IMAGES + '/images/profile-pictures/sean.jpeg';
+	hamMenuURL = `${HAM_MENU}#hamMenu`;
+	hamMenuCloseURL = `${HAM_MENU_CLOSE}#hamMenuClose`;
+
+	@track menuOpen = false;
+	imgSrc = this.hamMenuURL;
+	imgAlt = 'hamburger menu';
+
 	sections = [
 		{ Id: 1, Name: 'home' },
 		{ Id: 2, Name: 'about' },
 		{ Id: 3, Name: 'experience' },
-		{ Id: 4, Name: 'education' },
+		//{ Id: 4, Name: 'education' },
 		{ Id: 5, Name: 'projects' }
 	];
 
-	seanProfile = IMAGES + '/images/profile-pictures/sean.jpeg';
+	get displayDropDownMenu() {
+		console.log("TESTINg");
+		return !this.menuOpen ? 'header__sm-menu' : 'header__sm-menu header__sm-menu--active';
+	}
+	
+	handleMenuOpen() {
+		this.menuOpen = this.menuOpen ? false : true;
+
+		if (this.menuOpen) {
+			this.imgSrc = this.hamMenuCloseURL;
+			this.imgAlt = 'hamburger menu close';
+		} else {
+			this.imgSrc = this.hamMenuURL;
+			this.imgAlt = 'hamburger menu';
+		}
+	}
 
 	handleLinkClick(event) {
 		event.preventDefault();
