1d25999126037098fddb045b080e3d97bef2bd39
Sean DesMarteau <sean.desmarteau@gusto.com>
2023-01-27 08:59:52 -0700

    Finishing Footer

--- force-app/main/default/lwc/portfolioFooter/portfolioFooter.js
+++ force-app/main/default/lwc/portfolioFooter/portfolioFooter.js
@@ -1,3 +1,17 @@
 import { LightningElement } from 'lwc';
+import PNG from '@salesforce/resourceUrl/png';
+export default class PortfolioFooter extends LightningElement {
+    trailhead = PNG + '/png/trailhead2-ico.png';
+    linkedin = PNG + '/png/linkedin-ico.png';
+    github = PNG + '/png/github-ico.png';
+    twitter = PNG + '/png/twitter-ico.png';
+    instagram = PNG + '/png/insta-ico.png';
 
-export default class PortfolioFooter extends LightningElement {}
+    socials = [
+		{ Id: 1, Name: 'Trailhead', src: this.trailhead, url: 'https://trailblazer.me/id/sdesmarteau'},
+		{ Id: 2, Name: 'LinkedIn', src: this.linkedin, url: 'https://www.linkedin.com/in/seandesmarteau'},
+		{ Id: 3, Name: 'Github', src: this.github, url: 'https://github.com/DeMart2416'},
+		{ Id: 4, Name: 'Twitter', src: this.twitter, url: 'https://twitter.com/DeMart2416'},
+		{ Id: 5, Name: 'Instagram', src: this.instagram, url: 'https://www.instagram.com/demart2416'}
+	];
+}
